23134fa90e942234a795f6c25f40d7a95ca3e184
ceasaro <ceesvw@gmail.com>
2013-04-25 21:28:15 +0200

    added production settings and update wsgi.py

---
+++ jeslee_web/settings/production.py
@@ -0,0 +1,72 @@
+# Django settings for healthMap project.
+from jeslee_web.settings.defaults import *
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'jeslee',                      # Or path to database file if using sqlite3.
+        'USER': 'jeslee_geb',                      # Not used with sqlite3.
+        'PASSWORD': 'Ge*3Gdh!23>q',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': 'accounts_dev',                      # Or path to database file if using sqlite3.
+#        'USER': 'accounts_dev',                      # Not used with sqlite3.
+#        'PASSWORD': 'accounts_dev',                  # Not used with sqlite3.
+#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#    }
+}
+
+_dev_apps = list(INSTALLED_APPS)
+_dev_apps.append('debug_toolbar')
+_dev_apps.append('debug_toolbar_user_panel')
+#_dev_apps.append('django_nose')
+#_dev_apps.append('rosetta')
+INSTALLED_APPS = tuple(_dev_apps)
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+            },
+#        'mail_admins': {
+#            'level': 'ERROR',
+#            'class': 'django.utils.log.AdminEmailHandler'
+#        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'accounts_web': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    }
+}
