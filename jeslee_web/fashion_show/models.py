0b388ed3b2f85589d5259b98d7cb39d9a382f7a6
ceasaro <ceesvw@gmail.com>
2014-02-15 21:39:22 +0100

    add some style and i18n for registration form

--- jeslee_web/fashion_show/models.py
+++ jeslee_web/fashion_show/models.py
@@ -1,3 +1,4 @@
+from django.utils.translation import ugettext as _
 from django.db import models
 
 # Create your models here.
@@ -9,14 +10,14 @@ class Registration(TimeStampedModel):
     class Meta:
         abstract = True
 
-    name = models.CharField(u'name', max_length=50)
-    birth_date = models.DateField(u'birth date', max_length=10)
-    email = models.CharField(u'email', max_length=100)
-    street = models.CharField(u'street', max_length=100)
-    street_nr = models.CharField(u'street number', max_length=10)
-    zip = models.CharField(u'zip', max_length=100)
-    city = models.CharField(u'city', max_length=100)
-    country = models.CharField(u'country', max_length=100, default='Netherlands')
+    name = models.CharField(_(u'name'), max_length=50)
+    birth_date = models.DateField(_(u'birth date'), max_length=10)
+    email = models.CharField(_(u'email'), max_length=100)
+    street = models.CharField(_(u'street'), max_length=100)
+    street_nr = models.CharField(_(u'street number'), max_length=10)
+    zip = models.CharField(_(u'zip'), max_length=100)
+    city = models.CharField(_(u'city'), max_length=100)
+    country = models.CharField(_(u'country'), max_length=100, default='Netherlands')
 
 
 class FashionRegistration(Registration):
