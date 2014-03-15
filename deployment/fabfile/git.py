fe715dfabf6d4fb14bfa6d2ad9735a67917acfcd
ceasaro <ceesvw@gmail.com>
2014-03-15 19:03:31 +0100

    enable to check a specific git branch to deploy to server

--- deployment/fabfile/git.py
+++ deployment/fabfile/git.py
@@ -19,7 +19,7 @@ def pack(path=None, out_file_path=None):
     with TmpDirectory(prefix=tmp_prefix) as tmp_path:
         # TODO: validate repository and path
         output_dir = os.path.join(tmp_path, 'export')
-        clone(env.repository.url, output_dir)
+        clone(env.repository, output_dir)
         tar(output_dir, out_file_path)
     env.packed_file = out_file_path
 
@@ -36,10 +36,12 @@ def tar(directory, out_filepath):
         dir=directory, out=out_filepath))
 
 
-def clone(git_url, output_dir):
+def clone(repo, output_dir):
     local('mkdir -p {}'.format(output_dir))
     local('cd {}'.format(output_dir))
-    local('git clone {repo} {output_dir}'.format(
-        repo=git_url,
-        output_dir=output_dir))
+
+    local('git clone {repo} {branch} {output_dir}'.format(
+        repo=repo.url,
+        output_dir=output_dir,
+        branch="-b {}".format(repo.branch) if repo.branch else ""))
 
