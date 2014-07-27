8f5c899e40d0ddaee93a756df2a5dc2dfaf932be
ceasaro <ceesvw@gmail.com>
2014-07-27 13:25:18 +0200

    for some awkward and not understandable reason the url bookkeeping didn't work and financial does????

--- jeslee_web/urls.py
+++ jeslee_web/urls.py
@@ -53,7 +53,6 @@ urlpatterns = patterns('',
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': "auth/login.html"}, name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': "/"}, name='logout'),
 
-    url(r'^bookkeeping/', include('bookkeeping.transactions.urls')),
 
 )
 
@@ -78,6 +77,6 @@ urlpatterns += patterns("",
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(DIRNAME, "media"), 'show_indexes': True }),
 )
 
-# urlpatterns += patterns("",
-#     (r'^bookkeeping/', include('bookkeeping.transactions.urls')),
-#     )
+urlpatterns += patterns("",
+    (r'^financial/', include('bookkeeping.transactions.urls')),
+    )
