e35a91c509f5c11787707928b6bf237a50b844e6
ceasaro <ceesvw@gmail.com>
2014-07-27 13:14:32 +0200

    try an other url configuration

--- bookkeeping/transactions/urls.py
+++ bookkeeping/transactions/urls.py
@@ -2,11 +2,10 @@ from django.conf.urls import patterns, url
 
 from bookkeeping.transactions.views import PaymentCreateView, ClientCreateView, PaymentOverviewView, CategoryCreateView
 
-
 __author__ = 'ceasaro'
 urlpatterns = patterns(
-    '',
-    url(r'^/?$', PaymentOverviewView.as_view(template_name='overview.html'),
+    'bookkeeping.transactions',
+    url(r'^$', PaymentOverviewView.as_view(template_name='overview.html'),
         name='bookkeeping_home'),
     url(r'^payment/new$', PaymentCreateView.as_view(template_name='payment_new.html'),
         name='new_payment'),
--- jeslee_web/urls.py
+++ jeslee_web/urls.py
@@ -53,6 +53,7 @@ urlpatterns = patterns('',
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': "auth/login.html"}, name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': "/"}, name='logout'),
 
+    url(r'^bookkeeping/', include('bookkeeping.transactions.urls')),
 
 )
 
@@ -77,6 +78,6 @@ urlpatterns += patterns("",
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(DIRNAME, "media"), 'show_indexes': True }),
 )
 
-urlpatterns += patterns("",
-    (r'^bookkeeping/', include('bookkeeping.transactions.urls')),
-    )
+# urlpatterns += patterns("",
+#     (r'^bookkeeping/', include('bookkeeping.transactions.urls')),
+#     )
