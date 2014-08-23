87d06e247f12653753b70f1f88df0c7beaee09be
ceasaro <ceesvw@gmail.com>
2014-08-23 21:10:22 +0200

    download invoice as pdf

--- bookkeeping/invoice/urls.py
+++ bookkeeping/invoice/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
-from bookkeeping.invoice.views import InvoiceCreateView
+from bookkeeping.invoice.views import DownloadInvoiceView
 
 
 urlpatterns = patterns(
     'bookkeeping.invoice',
     url(r'^$', TemplateView.as_view(template_name='bookkeeping/invoice/overview.html'),
         name='invoice_home'),
-    url(r'^create/$', InvoiceCreateView.as_view(template_name='bookkeeping/invoice/overview.html'),
+    url(r'^create/$', DownloadInvoiceView.as_view(template_name='bookkeeping/invoice/overview.html'),
         name='invoice_create'),
 
 )
--- bookkeeping/invoice/views.py
+++ bookkeeping/invoice/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import TemplateView
+from django.core.files.base import ContentFile
+from django.http import HttpResponse
+
+from django.views.generic import  View
 from lfs.order.models import Order
 
 from bookkeeping.bookkeeping_core.models import Client
@@ -8,11 +11,15 @@ from bookkeeping.invoice.pdf import invoice_to_PDF
 __author__ = 'ceasaro'
 
 
-class InvoiceCreateView(TemplateView):
+class DownloadInvoiceView(View):
 
     def get(self, request, *args, **kwargs):
         client = Client.objects.all()[0]
-        order = Order.objects.get(id=1)
-        invoice_to_PDF(client=client, order=order)
-        return super(InvoiceCreateView, self).get(request, *args, **kwargs)
+        order = Order.objects.all()[0]
+        invoice_data = invoice_to_PDF(client=client, order=order)
+        pdf_file = ContentFile(invoice_data)
+        response = HttpResponse(pdf_file, mimetype="application/pdf")
+        response['Content-Length'] = pdf_file.size
+        response["Content-Disposition"] = "attachment; filename=factuur_jeslee_{0}.pdf".format(order.number)
+        return response
 
