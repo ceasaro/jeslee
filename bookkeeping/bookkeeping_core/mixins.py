8b315d4de5183b592429bca3660a5bd9b0a9ef9f
ceasaro <ceesvw@gmail.com>
2014-12-28 11:45:40 +0100

    keep selected year in session.

--- bookkeeping/bookkeeping_core/mixins.py
+++ bookkeeping/bookkeeping_core/mixins.py
@@ -10,8 +10,18 @@ class FinancialYearMixin(object):
         The financial year bound to the request as 'request.financial_year'
         return: the financial year form to request GET params if not found the current year is returned.
         """
+        import pdb
+        pdb.set_trace()
         this_year = date.today().year
-        financial_year = int(self.request.GET.get('year', this_year))
+        requested_year = self.request.GET.get('year', None)
+        if requested_year:
+            requested_year = int(requested_year)
+            self.request.session['financial_year'] = requested_year
+            financial_year = requested_year
+        else:
+            session_year = self.request.session['financial_year']
+            financial_year = session_year if session_year else int(this_year)
+
         self.request.financial_year = financial_year
-        self.request.financial_years = range(this_year-5, this_year+1)
+        self.request.financial_years = range(this_year-6, this_year+1)
         return financial_year
