9535e956eb3b467d15a211a8d27d30894b647c63
ceasaro <ceesvw@gmail.com>
2015-04-06 18:09:22 +0200

    get url from server

--- jeslee_web/static/js/pages_free/measure_explanation.js
+++ jeslee_web/static/js/pages_free/measure_explanation.js
@@ -16,11 +16,17 @@ $(document).ready(function() {
 
   $('.measure_title').on('click', function() {
     var url = $generated_measuer_url_element.data('measure-url'),
-      $clicked_title = $(this);
-    $clicked_title.toggleClass('selected_for_url');
+      $clicked_title = $(this),
+      $clicked_measure = $clicked_title.closest('.measure_explanation');
+    $clicked_measure.toggleClass('selected_for_url');
 
-    var $selected_measures = $('.selected_for_url');
-    $.getJSON(url, get_data).done(function( data ) {
+    var measure_ids = [];
+    $('.selected_for_url').each(function( index ) {
+      measure_ids.push($(this).data('measure-id'));
+      console.log($(this).data('measure-id'));
+    });
+    jQuery.ajaxSettings.traditional = true; // prevent jquery to add extra '[]'  (open- and close-bracket characters) as suffix to the parameter name
+    $.getJSON(url, {'m': measure_ids}).done(function( data ) {
       url = data['url'];
       console.log('url = '+url);
       $generated_measuer_url_element.find('.direct_url').html(url)
